84c90afbd07d7b76d4122ea12ce6a246591dc211
hackerzvibez <211648632+hackerzvibez@users.noreply.github.com>
2025-09-25 13:10:56 +0530

    implemented a code to calculate a sum of the elements inside an array

--- my_c++_programs/sum_array.cpp
+++ my_c++_programs/sum_array.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+#include <vector>
+
+int sumArray(std::vector<int> &v)
+{
+    int size = v.size();
+    int index = 0;
+    int total = 0;
+    for (index; index < size; index++)
+    {
+        total = total + v[index];
+    }
+    return total;
+}
 
 int main()
 {
+    std::vector<int> v = {23, 45, 59, 69, 82, 73, 95, 46, 15, 19, 1};
+
+    std::cout << "The sum of the Array is :" << sumArray(v) << std::endl;
 
     return 0;
 }
